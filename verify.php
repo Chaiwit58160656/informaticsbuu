3169aca60a067e89e6da7422c98727b19ff956f6
Chaiwit58160656 <36035072+Chaiwit58160656@users.noreply.github.com>
2019-01-24 15:52:37 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
+$access_token = '6AHRcWetHgf0rYxUUTGKVYeLmyztVD1ontI5HsAiuG7oRR61IZ1iS1ZombwCifqKVoTBhRftQ0n6Syors2QuO1MtV6DW5ZFFzDiK6aytBvlFa7hLeWrYH+1cs50rBaxXpQSXJYbKCIGJysLDu50BvwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
